74c0f3de7447229b9e66f0a0a5ad54cb18f055ee
Fantix King <fantix.king@gmail.com>
2017-10-13 17:15:37 +0800

    WIP

--- gino/__init__.py
+++ gino/__init__.py
@@ -1,7 +1,6 @@
 from .api import Gino
-from .pool import GinoPool
-from .connection import GinoConnection
 from .local import get_local, enable_task_local, disable_task_local
 from .exceptions import *
+from .strategies import create_engine
 
 __version__ = '0.5.4'
